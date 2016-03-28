d144ea48c0f5d253686bde5c1e90384b01030320
Bill Welliver <none@none>
2016-03-28 17:55:19 +0000

    changes

--- acconfig.h
+++ acconfig.h
@@ -5,6 +5,8 @@
 /* acconfig.h.  Generated from acconfig.h.in by configure.  */
 /* acconfig.h.in.  Generated from configure.in by autoheader.  */
 /* acconfig.h.  Generated from acconfig.h.in by configure.  */
+/* acconfig.h.in.  Generated from configure.in by autoheader.  */
+/* acconfig.h.  Generated from acconfig.h.in by configure.  */
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
@@ -47,6 +49,24 @@
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
 #define PACKAGE_VERSION ""
